e47615ded257dba39cae0cca55eb28163b5ddc07
clement_genty <84876568+Aheuf@users.noreply.github.com>
2026-06-19 16:37:31 +0200

    add: routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router/internal/react-server-client'
 import './App.css'
+import Niveau from './presentation/pages/niveau'
+import type { NiveauEnum } from './domain/models/Question'
 
 function App() {
 
   return (
-    <p>Hello world</p>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Introduction/>} /> */}
+        <Route path="/Niveau" element={<Niveau setPlayerLevel={(level:NiveauEnum) => {}}/>} />
+        {/* <Route path="/Questionnaire" element={<Questionnaire/>} /> */}
+        {/* <Route path="/Resultats" element={<Resultats/>} /> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
